fe2e075c304c18d36fb2f6729f982f9fb0d3c4f8
Errno404 <104851054+Errno-404@users.noreply.github.com>
2023-04-07 21:49:58 +0200

    Created tiles

--- src/com/mezydlo/JavaGame/graphics/Screen.java
+++ src/com/mezydlo/JavaGame/graphics/Screen.java
@@ -1,30 +1,38 @@
 package com.mezydlo.JavaGame.graphics;
 
+import java.util.Random;
+
 public class Screen {
     private int width, height;
     public int[] pixels;
+    public int[] tiles = new int[64 * 64]; // 64 tiles by 64 tiles
+    private Random random = new Random();
 
-
-    public Screen(int width, int height){
+    public Screen(int width, int height) {
         this.width = width;
         this.height = height;
         pixels = new int[width * height];
+
+        for (int i = 0; i < 64 * 64; i++) {
+            tiles[i] = random.nextInt(0xffffff);
+        }
+
+
     }
 
 
-    public void render(){
+    public void render() {
 
-        for(int y = 0; y < height; y++){
-
-            for(int x = 0 ; x < width; x++){
-
-                pixels[x + y *width] = 0xff00ff;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int tileIndex = (x >> 4) + (y >> 4) * 64; // finding the correct tile - each tile is 32 x 32, tiles = 64
+                pixels[x + y * width] = tiles[tileIndex]; // rendering the color
             }
         }
     }
 
-    public void clear(){
-        for(int i = 0; i < pixels.length; i++){
+    public void clear() {
+        for (int i = 0; i < pixels.length; i++) {
             pixels[i] = 0;
         }
     }
